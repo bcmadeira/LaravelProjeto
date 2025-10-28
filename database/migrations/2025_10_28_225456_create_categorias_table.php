0335edc684929d8239b3136132fb3ea4ca0bf312
Bruno Madeira <brunomadeira044@gmail.com>
2025-10-28 20:07:29 -0300

    Criação de Migration de Categorias

--- database/migrations/2025_10_28_225456_create_categorias_table.php
+++ database/migrations/2025_10_28_225456_create_categorias_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('categorias', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('nome', 100);
         });
     }
 
